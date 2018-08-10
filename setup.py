59752627ac9bd3dd8b7aa8af97e4b1be237b1fdf
Villalongue Maxime <maxime.villalongue@gmail.com>
2018-08-11 01:42:13 +0200

    Update setup.py
    add html

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     version='0.1',
     py_modules=['mailcheck'],
     install_requires=[
-        'Click','pyzmail','imapclient',
-    ],
+        'Click','pyzmail','imapclient',  'pprint','jinja2' , 'datetime' ,'imgkit'  ],
     entry_points='''
         [console_scripts]
         mailcheck=mailcheck:mailcheck
